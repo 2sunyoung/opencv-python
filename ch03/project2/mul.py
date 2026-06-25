f07c599344d232f3e216189c85af3deeb7cd27ed
2sunyoung <can75607560@gmail.com>
2026-06-25 14:54:01 +0900

    Create mul.py

---
+++ ch03/project2/mul.py
@@ -0,0 +1,11 @@
+import numpy as np
+A = np.array([[1,2,3],[-1,3,-10]])
+B = np.array([[2,-1],[3,1],[0,5]])
+AB = np.zeros((A.shape[0], B.shape[1])) #행 0 열 1
+for i in range(A.shape[0]):
+    for j in range(B.shape[1]):
+        for k in range(B.shape[0]):
+            AB[i][j] += A[i][k] * B[k][j]
+
+print(AB)
+
